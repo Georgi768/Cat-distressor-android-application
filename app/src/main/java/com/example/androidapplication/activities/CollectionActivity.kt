70a02c2602f1ad9e5df79d4af80cde49978e26b0
Christof145 <43606559+Christof145@users.noreply.github.com>
2022-03-24 08:25:45 +0100

    Add WIP adapter to update display
    Add cat_item to display

--- app/src/main/java/com/example/androidapplication/activities/CollectionActivity.kt
+++ app/src/main/java/com/example/androidapplication/activities/CollectionActivity.kt
@@ -1,7 +1,26 @@
 package com.example.androidapplication.activities
 
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.androidapplication.R
+import com.example.androidapplication.adapters.CatsAdapter
+import com.example.androidapplication.models.Cat
+
 
 class CollectionActivity : AppCompatActivity() {
+    private lateinit var cats: ArrayList<Cat>
+    private lateinit var mCatsAdapter: CatsAdapter
+    private lateinit var catImageView: ImageView
+    private lateinit var button: Button
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collection)
+        button.setOnClickListener {
+            mCatsAdapter.addCat(Cat("AAA", "aSHSAOID"))
+            mCatsAdapter.notifyDataSetChanged()
+        }
+    }
 }
